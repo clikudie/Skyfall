52d73145a2e1f9a195befd81ae1620895cce62f3
Constant Likudie <likudie@gmail.com>
2021-12-31 19:18:24 -0800

    using ocelot as API gateway

---
+++ Skyfall.Gateway/Program.cs
@@ -0,0 +1,20 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+await app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
